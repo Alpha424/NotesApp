3db8cfb33d0e1879309d609cf0aa9b1d6231b5ed
Alexander <Alexander@ALEXANDERPC>
2016-11-09 18:56:06 +0300

    Добавьте файлы проекта.

---
+++ NotesApp/MainWindow.xaml.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Linq;
+using System.Windows;
+
+namespace NotesApp
+{
+    public partial class MainWindow : Window
+    {
+        private DBModelDataContext dataContext = null;
+        public MainWindow()
+        {
+            InitializeComponent();
+            dataContext = new DBModelDataContext();
+        }
+
+        private void loginButton_Click(object sender, RoutedEventArgs e)
+        {
+            var result = from user in dataContext.users
+                where
+                user.username == username_field.Text && user.password == password_field.Password
+                select user;
+            
+            if (result.Count() > 0)
+            {
+                MessageBox.Show("SUCCESS!");
+            }
+            else
+            {
+                MessageBox.Show("Неправильный логин или пароль!", "");
+            }
+            
+        }
+    }
+}
